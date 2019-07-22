e74392b474add576ec219dd477d4239b90b36462
Jason Graziano <mrehawk82@gmail.com>
2019-07-22 15:17:33 -0400

    ...

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,7 @@ userdata = {
 	dBool: false,
 	planet_size: 1,
 	income: 0,
-	neutron: 1,
+	neutron: 3,
 	proton: 0,
 	electron: 0,
 	photon: 0,
@@ -25,6 +25,7 @@ userdata = {
 	beings: 0,
 	spaceship: 0
 };
+
 worlddata = {
 	mass: 0,
 	status: "HIGGS",
@@ -32,6 +33,7 @@ worlddata = {
 	spin: 0,
 	climate: "",
 	events: {},
+	debris: {},
 	log: {}
 };
 myUI = {
@@ -40,14 +42,14 @@ myUI = {
 		LSinit("worldData", worlddata);
 		LSinit("userData", userdata);
 		var uuu = parseLS("userData");
-		var www = parseLS("worldData");
+		var vvv = parseLS("worldData");
 
 		h = myHeight();
 		w = myWidth();
 
-		myUI.loader(h,w,uuu,www);
+		myUI.loader(h,w,uuu,vvv);
 	},
-	loader: (h,w,uuu,www) => {
+	loader: (h,w,uuu,vvv) => {
 		var dp = createEle("div"), timerCase = createEle("div"), 
 		    timerIn = createEle("input"), timerLabel = createEle("label"),
 		    myWorld = createEle("div");
@@ -108,12 +110,12 @@ myUI = {
 				bool = false;
 			}
 
-        	addBtns.onclick = myUI.senseBtns(addBtns,i,addBtnNames,uuu,www);
- 			addBtns.onmouseover = myUI.overOn(addBtns,i,addBtnNames,uuu,www);
+        	addBtns.onclick = myUI.senseBtns(addBtns,i,addBtnNames,uuu,vvv);
+ 			addBtns.onmouseover = myUI.overOn(addBtns,i,addBtnNames,uuu,vvv);
  			addBtns.innerHTML = addBtnNames[i];
  			addBtns.disabled = bool;
 
-			addBtns.className = "gBtns";
+			addBtns.className = "addBtns";
 
         	dp.append(addBtns);
         }
@@ -121,22 +123,22 @@ myUI = {
 
         myWorld.innerHTML = "&nbsp;";
         myWorld.className = "myWorld";
-        myWorld.id = www.status;
+        myWorld.id = vvv.status;
         myWorld.style.height = uuu.planet_size + "px";
         myWorld.style.width = uuu.planet_size + "px";
         myWorld.style.left = +((w / 2) - (uuu.planet_size / 2)) + "px";
         myWorld.style.top = +((h / 2) - (uuu.planet_size / 2)) + "px";
-        myWorld.onmouseover = myUI.planetLookUp(myWorld,uuu,www);
+        myWorld.onmouseover = myUI.planetLookUp(myWorld,uuu,vvv);
 
 		dvContain.append(dp,timerCase,myWorld);
 	},
-	planetLookUp: function(myWorld,uuu,www){
+	planetLookUp: function(myWorld,uuu,vvv){
 		return function(){
 			var popup = createEle("span"),
 			    x = event.clientX,
 			    y = event.clientY;
 
-			popup.innerHTML = "<p>SIZE:" + uuu.planet_size + "</p><p>TYPE: "+ www.status + "</p>";
+			popup.innerHTML = "<p>SIZE:" + uuu.planet_size + "</p><p>TYPE: "+ vvv.status + "</p>";
 			popup.style.position = "fixed";
 			popup.className = "popup";
 			popup.style.left = (x + 10) + "px";
@@ -148,7 +150,7 @@ myUI = {
 			//console.log(uuu.planet_size);
 		}
 	},
-	overOn: function(addBtns,i,addBtnNames,uuu,www){
+	overOn: function(addBtns,i,addBtnNames,uuu,vvv){
 		return function(){
 			var popup = createEle("span"),
 			    x = event.clientX,
@@ -193,9 +195,9 @@ myUI = {
 			popup.remove();
 		}
 	},
-	neutronSwipe: function(addBtns,i,addBtnNames,uuu,www){
+	neutronSwipe: function(addBtns,i,addBtnNames,uuu,vvv){
 		var dp = addBtns.parentNode, dvContain = dp.parentNode, body = dvContain.parentNode;
-		var myWorld = bySel("#" + www.status);
+		var myWorld = bySel("#" + vvv.status);
 		takeFull(dp);
 
 		function Thing(x, y){
@@ -203,11 +205,14 @@ myUI = {
 			this.y = y;
 		}
 
-		var mousePos = new Thing(0, 0);
+		var mousePos = new Thing(100, 100);
 	    var mycanvas = createEle("div");
 
 	    mycanvas.id = "mycanvas";
-    
+    	mycanvas.innerHTML = addBtns.innerHTML;
+	    mycanvas.style.top = mousePos.y + "px";
+	    mycanvas.style.left = mousePos.x + "px";
+
 	    body.addEventListener('mousemove', function(event){
 			mousePos.x = event.clientX;
 			mousePos.y = event.clientY;
@@ -216,16 +221,71 @@ myUI = {
 	        mycanvas.style.top = mousePos.y + "px";
 	        mycanvas.style.left = mousePos.x + "px";
 
-		}, false);
-		body.addEventListener('mousedown', function(event){
+		}, true);
+		
+		body.addEventListener('mousedown', myUI.placeUnit, true);
 
-		}, false);
 	    body.style.cursor = 'none';
 		body.append(mycanvas);
 		myWorld.disabled = true;
-		//console.log(myWorld)
+
+		//body.onclick = function(){ myUI.placeUnit(event,mycanvas,myWorld,addBtns,i,addBtnNames,uuu,vvv)};
+		//console.log(myWorld);
 	},
-	senseBtns: function(addBtns,i,addBtnNames,uuu,www){
+	placeUnit: function(event){
+			var unit = createEle("div"),
+				mycanvas = bySel("#mycanvas"),
+				uuu = parseLS("userData"),
+				vvv = parseLS("worldData"),
+				addBtns = bySelAll(".addBtns"),
+				addBtnNames = bySelAll(".addBtnNames"),
+				dp = bySel(".dp"),
+				myWorld = bySel("#" + vvv.status),bkgnd,c;
+
+			for (var b = 0; b < addBtns.length; b++) {
+				if(b === 1){
+					uuu.neutron = uuu.neutron - 1;
+					bkgnd = "darkgrey";
+				    if(uuu.neutron <= 0){
+				    	addBtns[b].disabled = true;
+				    }
+				    c = b;
+				}
+	
+			}
+
+			for (var d = 0; d < [vvv.debris].length; d++) {
+				var dTemplate = [
+					key = d++,
+					x = event.clientX,
+					y = event.clientY,
+					name = btnLabel[c]
+				];
+				vvv.debris[d] = dTemplate;
+				//console.log(d);
+			}
+
+			saveLS("userData", uuu);
+			saveLS("worldData", vvv);
+
+			unit.innerHTML = "nbsp;";
+			unit.className = "unit";
+			unit.style.backgroundColor = bkgnd;
+			unit.style.top = event.clientY + "px";
+	        unit.style.left = event.clientX + "px";
+
+			body.style.cursor = 'default';
+			
+			body.append(unit);
+			
+			mycanvas.remove();
+			myWorld.disabled = false;
+
+			makeFull(dp);
+
+			body.removeEventListener('mousedown', myUI.placeUnit, true);
+	},
+	senseBtns: function(addBtns,i,addBtnNames,uuu,vvv){
 		return function(){
 			var timerIn = bySel(".timerIn");
 			if(i === 0){
@@ -238,7 +298,7 @@ myUI = {
             	}
 			}
 			if(i === 1){
-				myUI.neutronSwipe(addBtns,i,addBtnNames,uuu,www);
+				myUI.neutronSwipe(addBtns,i,addBtnNames,uuu,vvv);
 			}
 			if(i === 2){
 
@@ -267,7 +327,7 @@ myUI = {
 					function loop(uuu){
 						setTimeout(function(){
 							uuu.day++;
-							saveLS("userData",uuu,www);
+							saveLS("userData",uuu,vvv);
 							timerIn.value = uuu.day;
 							addBtns.innerHTML = "▶";
 							addBtns.disabled = false;
